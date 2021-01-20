a51391d9cf3602851d485ab3a4b6568d42c8bab2
H. Muller <hxmuller@gmail.com>
2021-01-20 17:46:13 -0500

    Add data and player choice

--- adventure_game.py
+++ adventure_game.py
@@ -1,3 +1,21 @@
+import time
+
+# Intro text
 print("Your stomach starts to grumble.")
+time.sleep(1.5)
 print("You walk over to the freezer, open it and look in.")
-print("The freezer is empty, time to hunt!")
+time.sleep(1.5)
+print("The freezer is empty, time to hunt!\n")
+time.sleep(1.5)
+
+# Data to be generated randomly
+# Animals are {rabbit, deer, bull moose, black bear}
+# wind direction is the direction it is coming from.
+# distance is either close or far.
+data = ["rabbit", "left", "close"]
+
+# Player chooses weapon
+print("You open the gun safe and see your pistol and rifle.")
+data.append(input("Enter P to get the pistol\n"
+               "Enter R to get the rifle\n").lower())
+print(data[3])
